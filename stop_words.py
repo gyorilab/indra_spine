35da8c2bcf90599287e4a0528ca0486d89a10311
Sangeetha Vempati <svempati0921@gmail.com>
2024-10-07 09:43:35 -0400

    Load stop words from text files

--- stop_words.py
+++ stop_words.py
@@ -5,19 +5,6 @@ from nltk.corpus import stopwords
 # Add in stopwords
 sw_nltk = stopwords.words('english')
 
-false_phrases = [',', '(', 'II', '40%', ')', '2', 'MXII', 'IV', '60', '60%',
-                 '20%', '6', 'e.g', '7', '3', 'non', '65', '7-Mar', '17',
-                 '%', '29', '25', 'Ts2)']
+#false_phrases = []
 
-exclude_words = ['normal', 'eminence', 'targets', 'individual', 'principal',
-                 'facial', 'source', 'hair', 'several areas', 'factors',
-                 'muscle','accessory', 'part', 'origin', 'separation',
-                 'levels', 'stage', 'projections', 'dendrite', 'addition',
-                 'distinct regions', 'locations', 'system', 'brain',
-                 ' \'s disease', 'CNS', 'central nervous', 'cholinergic',
-                 'projection', 'lobe', 'lobule', 'brain', 'cortex', 'anterior',
-                 'posterior', 'lateral', 'medial', 'nucleus', 'superior',
-                 'inferior', 'projections', 'lobes', 'lobules', 'cortices',
-                 'dorsal', 'ventral', 'rostral', 'caudal', 'segment', 'area',
-                 'tract','formation', 'segments', 'areas', 'tracts',
-                 'formations', 'basal']
+#exclude_words = []
